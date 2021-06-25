1c7edfbddeac68667dc46aa5b0b6aecf41783bb6
Khryrystyna <82049807+Khryrystyna@users.noreply.github.com>
2021-06-25 15:25:05 +0300

    js pro

--- js/products.js
+++ js/products.js
@@ -2,7 +2,9 @@ const productsJson = `
 [
     {
         "id": "1",
-       
+        "text-link": "Cabernet",
+        "price": 17.00,
+        "imgUrl": "img/cabernet.jpeg"
     },
     {
         "id": "2",
@@ -15,47 +17,15 @@ const productsJson = `
     },
     {
         "id": "4",
+        "id": "2",
+        "title": "Banana",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga expedita obcaecati adipisci explicabo impedit facere est qui voluptate. Fugiat libero molestiae suscipit eaque quae nihil sequi esse numquam dolor nisi!",
+        "price": 12.99,
+        "imgUrl": "img/banana.svg"
+    },
        
     },
   
 ]
 `;
 
-function renderProducts(products, sortDirection) {
-    const sortedProducts = [...products].sort( (a, b ) => 
-        sortDirection === 'ascending' ? a.price - b.price : b.price - a.price
-    );
-    let productsHtml = '';
-    for (const product of sortedProducts) {
-        productsHtml += `
-            <article>
-                <img src="${product.imgUrl}" alt="${product.title}" />
-                <h3>${product.title}</h3>
-                <p>${product.description}</p>
-                <div class="buttons">
-                    <button class="button card-button">Info</button>
-                    <button class="button card-button">Buy - $${product.price}</button>
-                </div>
-        </article>`;
-    }
-    document.querySelector('.products').innerHTML = productsHtml;
-}
-
-const products = JSON.parse(productsJson);
-
-renderProducts(products, 'ascending');
-
-
-const btnSortAsc = document.querySelector('.sort-asc');
-const btnSortDesc = document.querySelector('.sort-desc');
-
-btnSortAsc.addEventListener('click', sortAscending);
-btnSortDesc.addEventListener('click', sortDescending);
-
-function sortAscending() {
-    renderProducts(products, 'ascending');
-}
-
-function sortDescending() {
-    renderProducts(products, 'descending');
-}
