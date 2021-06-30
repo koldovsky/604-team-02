621d8ed327b46b8a1ad31b0510d610d82af81ee8
Julie <85100500+julie-konanchuk@users.noreply.github.com>
2021-06-30 17:32:17 +0300

    accordion,carousel,icons

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,14 @@
-const accordions = document.querySelectorAll("accordion-item");
+const accordions = document.querySelectorAll(".accordion-item");
 
 for(item of accordions) {
     item.addEventListener("click", function(){
-        this.classListener.toggle("active")
+        if(this.classList.contains("active")) {
+            this.classList.remove('active');
+        } else {
+            for(el of accordions) {
+                el.classList.remove('active');
+            }
+               this.classList.add('active');
+        }
     });
 }
