322f6139ab864dff2445a554cb3be541e6f9fa7a
Khryrystyna <82049807+Khryrystyna@users.noreply.github.com>
2021-08-02 21:18:45 +0300

    ac
    ac

--- js/callback-form.js
+++ js/callback-form.js
@@ -1,70 +1,80 @@
-  const callbackForm = document.querySelector('.callback-form-container');
-  const requestReceivedModal = document.querySelector('#request-received');
+const submitBtn = document.querySelector('#modal-active');
+  const modalActive = document.querySelector("#request-received");
+
+  submitBtn.addEventListener('click', hide-show);
+  function hide-show (){
+      alert ('njhjj')
+  }; 
 
   
-  const userName = document.querySelector('#callback-form-input-name');
-  const userEmail = document.querySelector('#callback-form-input-email');
-  const userPhone = document.querySelector('#callback-form-input-phone');
+  
+// //   const callbackForm = document.querySelector('.callback-form-container');
+// //   const requestReceivedModal = document.querySelector('#request-received');
 
-userPhone.addEventListener('click', function() {
-    if (!userPhone.value.trim()) {
-        userPhone.value = '+380';
-    }
-});
+  
+//   const userName = document.querySelector('#callback-form-input-name');
+//   const userEmail = document.querySelector('#callback-form-input-email');
+//   const userPhone = document.querySelector('#callback-form-input-phone');
 
-userPhone.addEventListener('blur', function() {
-    if (userPhone.value === '+380') {
-        userPhone.value = '';
-    }
-});
+// // userPhone.addEventListener('click', function() {
+// //     if (!userPhone.value.trim()) {
+// //         userPhone.value = '+380';
+// //     }
+// // });
 
-callbackForm.addEventListener('submit', function(event){
-    event.preventDefault();
-    let hasError = false;
+// userPhone.addEventListener('blur', function() {
+//     if (userPhone.value === '+380') {
+//         userPhone.value = '';
+//     }
+// });
 
-    if (!userName.value.trim()) {
-        userName.classList.add('callback-form-input-error');
-        hasError = true;
-    } else {
-        userName.classList.remove('callback-form-input-error');
-    }
+// callbackForm.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     let hasError = false;
 
-    if (!userEmail.value.trim() || !isEmailValid(userEmail.value)) {
-        userEmail.classList.add('callback-form-input-error');
-        hasError = true;
-    } else {
-        userEmail.classList.remove('callback-form-input-error');
-    }
+//     if (!userName.value.trim()) {
+//         userName.classList.add('callback-form-input-error');
+//         hasError = true;
+//     } else {
+//         userName.classList.remove('callback-form-input-error');
+//     }
 
-    if (!userPhone.value.trim() || !isPhoneValid(userPhone.value)) {
-        userPhone.classList.add('callback-form-input-error');
-        hasError = true;
-    } else {
-        userPhone.classList.remove('callback-form-input-error');
-    }
+//     if (!userEmail.value.trim() || !isEmailValid(userEmail.value)) {
+//         userEmail.classList.add('callback-form-input-error');
+//         hasError = true;
+//     } else {
+//         userEmail.classList.remove('callback-form-input-error');
+//     }
 
-    if (hasError) {
-        return;
-    }
+//     if (!userPhone.value.trim() || !isPhoneValid(userPhone.value)) {
+//         userPhone.classList.add('callback-form-input-error');
+//         hasError = true;
+//     } else {
+//         userPhone.classList.remove('callback-form-input-error');
+//     }
 
-    userName.value = '';
-    userEmail.value = '';
-    userPhone.value = '';
+//     if (hasError) {
+//         return;
+//     }
 
-    requestReceivedModal.classList.add('modal-active');
+//     userName.value = '';
+//     userEmail.value = '';
+//     userPhone.value = '';
 
-    setTimeout(function() {
-        requestReceivedModal.classList.remove('modal-active');
-    }, 4000);
-});
+//     requestReceivedModal.classList.add('modal-active');
 
-  function isPhoneValid(phone = '') {
-    const regexp = /(\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4})/;
+//     setTimeout(function() {
+//         requestReceivedModal.classList.remove('modal-active');
+//     }, 4000);
+// });
 
-    return phone.match(regexp);
-}
+//   function isPhoneValid(phone = '') {
+//     const regexp = /(\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4})/;
 
-  function isEmailValid(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email.toLowerCase());
-}
+//     return phone.match(regexp);
+// }
+
+//   function isEmailValid(email) {
+//     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(email.toLowerCase());
+// }
